3a6043003e0fd94f9a3ea475a8daba3b186e3663
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-31 18:58:55 -0700

    corrected import mistakes

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, SignUpView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list')
+    path('', PostListView.as_view(), name='post_list'),
+    path('signup/', SignUpView.as_view(), name='signup')
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,9 +1,17 @@
 # from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.urls import reverse_lazy
+from django.views import generic
 from .models import Post
+from .forms import SignUpForm
 
 # Create your views here.
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name= 'posts'
+
+class SignUpView(generic.CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
